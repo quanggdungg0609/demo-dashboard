34fe7afae8604df35c8ec36d9bf0e917409418bc
Quang dung Truong <truongquangdung0609@gmail.com>
2025-05-26 14:47:36 +0200

    chore: remove eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // Tắt rule unused-vars
+      "@typescript-eslint/no-explicit-any": "off", // Nếu muốn tắt luôn rule cấm dùng any
+      "react-hooks/exhaustive-deps": "warn", // hoặc "off" nếu muốn tắt cảnh báo useEffect
+    },
+  },
 ];
 
 export default eslintConfig;
